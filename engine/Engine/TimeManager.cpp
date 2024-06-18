ed09a76be300f2c90aea519219414c842b60104e
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-06-18 18:53:36 +0100

    Added timer class

---
+++ engine/Engine/TimeManager.cpp
@@ -0,0 +1,44 @@
+#include "TimeManager.h"
+
+#include <glfw3.h>
+
+
+TimeManager::TimeManager(Window* window) :window{ window }, previousTime{}, currentTime{}, elapsedTime{}, frameCount{ 0 }
+{
+}
+
+void TimeManager::Start()
+{
+	elapsedTime = 0;
+	frameCount = 0;
+	previousTime = glfwGetTime();
+	currentTime = glfwGetTime();
+}
+
+void TimeManager::Tick()
+{
+	previousTime = currentTime;
+	currentTime = glfwGetTime();
+	elapsedTime += DeltaTime();
+	++frameCount;
+}
+
+double TimeManager::DeltaTime()
+{
+	return currentTime - previousTime;
+}
+
+double TimeManager::FPS()
+{
+	return frameCount / elapsedTime;
+}
+
+double TimeManager::MSPF()
+{
+	return 1000.0 / FPS();
+}
+
+double TimeManager::ElapsedTime()
+{
+	return elapsedTime;
+}
