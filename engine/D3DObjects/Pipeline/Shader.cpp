67b3d3830d1951f160c00221688e1557bf9478d5
Cassie Parsons <71268741+CassieParticles@users.noreply.github.com>
2024-09-18 15:22:36 +0100

    Added Shader class using templates

---
+++ engine/D3DObjects/Pipeline/Shader.cpp
@@ -0,0 +1,54 @@
+#include "Shader.h"
+
+#include <iostream>
+#include <d3dcompiler.h>
+#include <Engine/D3DObjects/Device.h>
+
+bool compileShader(const std::wstring& filePath, Microsoft::WRL::ComPtr<ID3DBlob>& shaderByteCode, const std::string& profile, const std::string& entryFunction)
+{
+	constexpr UINT compileFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+	Microsoft::WRL::ComPtr<ID3DBlob> tempShaderBlob = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
+
+	HRESULT errorCode = D3DCompileFromFile(filePath.data(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entryFunction.data(), profile.data(), compileFlags, 0, &tempShaderBlob, &errorBlob);
+	if (FAILED(errorCode))
+	{
+		if (errorCode == 0x80070003)
+		{
+			std::cerr << "Failed to find shader at path specified\n";
+		}
+		if (errorBlob != nullptr)
+		{
+			std::cerr << "Compilation message" << static_cast<const char*>(errorBlob->GetBufferPointer()) << "\n";
+		}
+		return false;
+	}
+	shaderByteCode = std::move(tempShaderBlob);
+	return true;
+}
+
+Shader<VERTEX_SHADER>::Shader(const std::string& name, const std::wstring& filePath) : name{ name }
+{
+	Device* device = Device::Instance();
+	if (compileShader(filePath, byteCode, "vs_5_0", "Main"))
+	{
+		if (FAILED(device->getDevice()->CreateVertexShader(byteCode->GetBufferPointer(), byteCode->GetBufferSize(), nullptr, &shader)))
+		{
+			std::cerr << "Failed to create shader\n";
+		}
+	}
+}
+
+Shader<PIXEL_SHADER>::Shader(const std::string& name, const std::wstring& filePath) : name{ name }
+{
+	Device* device = Device::Instance();
+	ComPtr<ID3DBlob> byteCode{};
+	if (compileShader(filePath, byteCode, "ps_5_0", "Main"))
+	{
+		if (FAILED(device->getDevice()->CreatePixelShader(byteCode->GetBufferPointer(), byteCode->GetBufferSize(), nullptr, &shader)))
+		{
+			std::cerr << "Failed to create shader\n";
+		}
+	}
+}
---
+++ engine/D3DObjects/Pipeline/Shader.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <wrl.h>
+#include <d3d11.h>
+
+#include <engine/D3DObjects/Pipeline/PipelineStages.h>
+
+template<Shaders T> class Shader{};
+
+template<>
+class Shader<VERTEX_SHADER>
+{
+	template<typename S>
+	using ComPtr = Microsoft::WRL::ComPtr<S>;
+public:
+	Shader(const std::string& name, const std::wstring& filePath);
+
+	const std::string& getName() { return name; }
+	ComPtr<ID3DBlob>& getByteCode() { return byteCode; }
+
+	void bindShader() { Device::Instance()->getDeviceContext()->VSSetShader(shader.Get(), nullptr, 0); }
+protected:
+	const std::string name;
+
+	ComPtr<ID3D11VertexShader> shader;
+	ComPtr<ID3DBlob> byteCode;
+};
+
+template<>
+class Shader<PIXEL_SHADER>
+{
+	template<typename S>
+	using ComPtr = Microsoft::WRL::ComPtr<S>;
+public:
+	Shader(const std::string& name, const std::wstring& filePath);
+
+	const std::string& getName() { return name; }
+
+	void bindShader() { Device::Instance()->getDeviceContext()->PSSetShader(shader.Get(), nullptr, 0); }
+protected:
+	const std::string name;
+
+	ComPtr<ID3D11PixelShader> shader;
+};
